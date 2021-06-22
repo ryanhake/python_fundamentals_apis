fb0961d53983a498be1dd0f95f5ea576626392f1
ryanhake <80708155+ryanhake@users.noreply.github.com>
2021-06-22 13:34:12 -0400

    Add files via upload

---
+++ mysql/mysql.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+from pprint import pprint
+
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:Ry72an95$@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+
+query = sqlalchemy.select([actor])
+result_proxy = connection.execute(query)
+
+result_set = result_proxy.fetchall()
+pprint(result_set)
